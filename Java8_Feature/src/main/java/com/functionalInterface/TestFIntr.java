dec50e3e9dd6bdc2e5dda0e50f2c2fefdab28844
Bhasker23 <shrimalibhaskar2398@gmail.com>
2023-12-09 17:24:42 +0530

    functional Interface example

---
+++ Java8_Feature/src/main/java/com/functionalInterface/TestFIntr.java
@@ -0,0 +1,15 @@
+package com.functionalInterface;
+
+public class TestFIntr{
+
+    public static void main(String[] args) {
+
+        FInterface find = new FInterface() {
+            @Override
+            public int calculateSquare(int a) {
+                return a*a;
+            }
+        };
+        System.out.println(find.calculateSquare(5));
+    }
+}
